27e98321a164c1dac061ee27b3ae7719c5f7a1ac
RudyBecker <52896529+RudyBecker@users.noreply.github.com>
2020-09-19 17:50:36 -0700

    second commit

--- src/components/SearchMovies.vue
+++ src/components/SearchMovies.vue
@@ -1,44 +1,46 @@
 <template>
-  <div class="movies">
-    <h1>Search Your Favorite Movies</h1>
-    <section>
+  <div>
+    <div class="collection">
+      <h3>My Collection of Movies</h3>
+      <div v-for="(movie, n) in movies" :key="`movie-${n}`">
+        <p>
+          <span class="movie">{{movie}}</span>
+          <button @click="removeMovie(n)">Remove</button>
+        </p>
+      </div>
+    </div>
+    <div class="movies">
+      <h1>Search Your Favorite Movies</h1>
       <form @submit.prevent="submitted">
         <input v-model="val" type="text" />
         <button>Submit</button>
       </form>
-    </section>
-    <div v-if="error">
-      <h2>erorr!! {{ error }}</h2>
+      <div v-if="error">
+        <h2>erorr!! {{ error }}</h2>
+      </div>
+      <div v-if="fetching">
+        <h2>Finding Movies...</h2>
+      </div>
+      <div class="search-results" v-for="movie in list.results" :key="movie.id">
+        <ul>
+          <li>
+            <span class="title">Movie Title:</span>
+            <span class="bold">{{ movie.title }}</span>
+            <button class="add-button" @click="addCollection(movie.title)">Add To Collection</button>
+          </li>
+          <li>
+            <span class="title">Release Date:</span>
+            <span class="bold">{{ movie.release_date }}</span>
+          </li>
+          <li>
+            <span class="title">Overview:</span>
+            <span class="bold clamp">{{ movie.overview }}</span>
+          </li>
+        </ul>
+      </div>
+      <span class="title">Total Search Results:</span>
+      <span class="bold">{{ list.total_results }}</span>
     </div>
-    <div v-if="fetching">
-      <h2>Finding Movies...</h2>
-    </div>
-    <h3>My Collection of Movies</h3>
-    <div v-for="(movie, n) in movies" :key="`movie-${n}`">
-      <p>
-        <span class="movie">{{movie}}</span>
-        <button @click="removeMovie(n)">Remove</button>
-      </p>
-    </div>
-    <div class="search-results" v-for="movie in list.results" :key="movie.id">
-      <ul>
-        <li>
-          <span class="title">Movie Title:</span>
-          <span class="bold">{{ movie.title }}</span>
-          <button class="add-button" @click="addCollection(movie.title)">Add To Collection</button>
-        </li>
-        <li>
-          <span class="title">Release Date:</span>
-          <span class="bold">{{ movie.release_date }}</span>
-        </li>
-        <li>
-          <span class="title">Overview:</span>
-          <span class="bold clamp">{{ movie.overview }}</span>
-        </li>
-      </ul>
-    </div>
-    <span class="title">Total Search Results:</span>
-    <span class="bold">{{ list.total_results }}</span>
   </div>
 </template>
 
@@ -77,6 +79,7 @@ export default createComponent({
   methods: {
     addCollection(movie) {
       this.movies.push(movie);
+      this.saveMovies();
     },
     removeMovie(x) {
       this.movies.splice(x, 1);
@@ -93,11 +96,11 @@ export default createComponent({
 <style scoped>
 .search-results {
   display: flex;
-  width: 600px;
+  width: 700px;
   text-align: left;
 }
 .movies {
-  max-width: 1024px;
+  max-width: 800px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -125,5 +128,16 @@ li {
   display: -webkit-box;
   -webkit-line-clamp: 3;
   -webkit-box-orient: vertical;
+}
+
+.collection {
+  height: 500px;
+  width: 300px;
+  position: fixed;
+  padding: 2em;
+  left: 10%;
+  top: 0%;
+  transform: translateX(-50%);
+  overflow-y: auto;
 }
 </style>
